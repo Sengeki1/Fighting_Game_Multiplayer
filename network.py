3c791f251d88576933cacb6e07c64a18ae247d9d
Sengeki1 <gamersengeki@gmail.com>
2024-05-20 22:45:19 -0100

    fixed server

--- network.py
+++ network.py
@@ -4,7 +4,7 @@ import pickle
 class Network:
     def __init__(self) -> None:
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.1.68"
+        self.server = "192.168.1.65"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p = self.connect()
@@ -14,13 +14,13 @@ class Network:
     def connect(self):
         try:
             self.client.connect(self.addr) # Connect to Server
-            return pickle.loads(self.client.recv(1024)) # get Player Position and Color
+            return pickle.loads(self.client.recv(4096)) # get Player Position and Color
         except:
             pass
 
     def send(self, data):
         try:
             self.client.send(pickle.dumps(data))
-            return pickle.loads(self.client.recv(1024))
+            return pickle.loads(self.client.recv(4096))
         except socket.error as e:
             print(e)
